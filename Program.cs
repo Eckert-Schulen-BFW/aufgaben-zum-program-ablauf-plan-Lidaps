7cf3c6549df6992e8a50b7ad98d13f32483cce44
Steffen Dimmerling <sdimmerling@gmail.com>
2026-04-01 16:18:12 +0200

    Aufgaben

--- Program.cs
+++ Program.cs
@@ -8,23 +8,95 @@
 // AUFGABE 1 – Beantworte folgende Fragen hier als Kommentar:
 //
 // Frage 1: Wie viele Entscheidungsknoten (Rauten) hat der PAP?
-// Antwort: 
+// Antwort: 6
 //
 // Frage 2: Unter welchen Bedingungen endet das Programm frühzeitig?
-// Antwort: 
+// Antwort: Wenn Tryparse false
 //
 // Frage 3: Warum wird TryParse statt Convert.ToDouble() verwendet?
-// Antwort: 
+// Antwort: Weil Tryparse überprüft also versucht zu parsen. Bei false wird false ausgegeben bei converttodouble würde das Programm abstürzen
 //
 // Frage 4: Welchen Datentyp sollen gewicht und groesse haben, und warum?
-// Antwort: 
+// Antwort: Double weil Kommazahlen
 //
 // ============================================================
 
 // TODO: Schreibe dein Programm unterhalb dieser Zeile.
 //       Orientiere dich dabei am PAP in der README.md.
 //       Jeder Schritt im PAP sollte einer Zeile / einem Block in deinem Code entsprechen.
-System.Console.WriteLine("test");
+using System;
+
+
+
+
+
+
+// Start
+// Ausgabe: Bitte Namen eingeben
+System.Console.WriteLine("Bitte Namen eingeben!");
+//Eingabe Name
+string? strname = Console.ReadLine();
+strname = strname!= null ? strname : "Falsche Eingabe";
+//Ausgabe
+System.Console.WriteLine("Bitte Gewicht in kg eingeben!");
+double doubleWeight = -1;
+string? strWeight = Console.ReadLine();
+bool boolWeight = double.TryParse(strWeight,out doubleWeight); 
+if (boolWeight = !true)
+{
+    Console.WriteLine("Fehler Ungültige \n Eingabe");
+    
+};
+
+Console.WriteLine("Bitte Größe in meter eingeben!");
+double dblGroese = -1;
+string? strGroese = Console.ReadLine();
+bool blGroese = double.TryParse(strGroese,out dblGroese);
+if (blGroese = false)
+{
+    Console.WriteLine("Fehler Ungültige \n Eingabe");
+    
+};
+
+
+if (dblGroese < 0)
+{
+    System.Console.WriteLine("Fehler \nGröße muss psitiv sein");
+
+    
+}
+// Berechnung BMI = Gewicht / (Größe x Größe)
+
+double dbBmi = -1;
+
+dbBmi = doubleWeight / (dblGroese * dblGroese);
+string kat ="";
+if (dbBmi < 18.5)
+{
+   kat ="Untergewicht";
+}
+else if (dbBmi < 25)
+{
+    kat ="Normalgewicht";
+}
+else if (dbBmi < 30)
+{
+    kat ="Übergewicht";
+}
+else
+{
+    kat ="Starkes Übergewicht";
+}
+
+
+
+
+
+
+
+
+
+
 // ── Schritt 1: Programmtitel ausgeben ───────────────────────
 // Tipp: Nutze Console.WriteLine() für die Titelbox.
 // TODO: Ausgabe der Titelzeile (wie im Beispiel in der README)
